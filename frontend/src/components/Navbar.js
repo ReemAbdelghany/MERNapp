c10c2106c1d159db9b30adbf6cf73135a1ba8292
ReemAbdelghany <127961912+ReemAbdelghany@users.noreply.github.com>
2024-01-08 19:13:52 +0200

    css changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,30 +1,41 @@
-import { Link } from 'react-router-dom'
-import { useLogout } from '../hooks/useLogout'
-import { useAuthContext } from '../hooks/useAuthContext'
+import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
-  const { logout } = useLogout()
-  const { user } = useAuthContext()
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleClick = () => {
-    logout()
-  }
+    logout();
+  };
+
+  const getEmailAlias = () => {
+    if (user) {
+      const atIndex = user.email.indexOf('@');
+      return atIndex !== -1 ? user.email.substring(0, atIndex) : user.email;
+    }
+    return '';
+  };
 
   return (
     <header>
       <div className="container">
-        <Link to="/">
-          <h1>My Workout Tracker</h1>
+        <Link to="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
+          <FontAwesomeIcon icon={faDumbbell} style={{ marginRight: '10px', fontSize: '1.5rem' }} />
+          <h1 style={{ fontSize: '1.5rem', margin: 0 }}>My Workout Tracker</h1>
         </Link>
         <nav>
           {user && (
             <div>
-              <span>{user.email}</span>
+              <span title={user.email}>{getEmailAlias()}</span>
               <button onClick={handleClick}>Log out</button>
             </div>
           )}
           {!user && (
-            <div>
+            <div className="lnks">
               <Link to="/login">Login</Link>
               <Link to="/signup">Signup</Link>
             </div>
@@ -32,7 +43,7 @@ const Navbar = () => {
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
